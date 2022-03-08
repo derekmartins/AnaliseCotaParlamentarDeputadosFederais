e2ac87e2283f991860debbed503e030ce7d01381
Laio <83889645+LaioGomes@users.noreply.github.com>
2022-03-08 18:56:02 -0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,2 +1,4 @@
 def carregar_dados():
-  pass
+  df_2019 = pd.read_csv('Ano-2019.csv', sep = ';')
+  df_2020 = pd.read_csv('Ano-2020.csv', sep = ';') 
+  df_2021 = pd.read_csv('Ano-2021.csv', sep = ';')
