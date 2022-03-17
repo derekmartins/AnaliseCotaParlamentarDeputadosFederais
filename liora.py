0995ed55dc360e23eb46f955f83b78ee792f926c
lioradopacio <liora.vanessa@gmail.com>
2022-03-16 21:54:50 -0300

    Atualizando funções essencias e markdowns na Análise Final

--- liora.py
+++ liora.py
@@ -1,24 +1,33 @@
+import pandas as pd
+import numpy as np
+
 def limpar_coluna_nome(df):
   df.nome = df.nome.str.strip()
   return df
 
-def df_2019():
+def carregar_df_2019():
     types = {
         'cpf': 'category',
         'vlrLiquido': 'float'
     }
     return pd.read_csv(r'C:\Users\liora\Downloads\CEAP2019.csv', sep=';', dtype=types)
 
-def df_2020():
+def carregar_df_2020():
     types = {
         'cpf': 'category',
         'vlrLiquido': 'float'
     }
     return pd.read_csv(r'C:\Users\liora\Downloads\CEAP2020.csv', sep=';', dtype=types)
 
-def df_2021():
+def carregar_df_2021():
     types = {
         'cpf': 'category',
         'vlrLiquido': 'float'
     }
     return pd.read_csv(r'C:\Users\liora\Downloads\CEAP2021.csv', sep=';', dtype=types)
+
+def dados_nulos(df):
+    return (df.isnull().sum() / df.shape[0]).sort_values(ascending=False)
+
+
+
