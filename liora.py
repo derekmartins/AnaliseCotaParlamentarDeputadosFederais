9edc22ed0167396693ee4f4a308764ef28fd250d
Liora Vanessa Dopacio <88158624+lioradopacio@users.noreply.github.com>
2022-03-16 21:24:02 -0300

    Update liora.py
    Funções para carregar arquivos em csv dos anos de 2019, 2020 e 2021 dos gastos parlamentares

--- liora.py
+++ liora.py
@@ -1,3 +1,24 @@
 def limpar_coluna_nome(df):
   df.nome = df.nome.str.strip()
   return df
+
+def df_2019():
+    types = {
+        'cpf': 'category',
+        'vlrLiquido': 'float'
+    }
+    return pd.read_csv(r'C:\Users\liora\Downloads\CEAP2019.csv', sep=';', dtype=types)
+
+def df_2020():
+    types = {
+        'cpf': 'category',
+        'vlrLiquido': 'float'
+    }
+    return pd.read_csv(r'C:\Users\liora\Downloads\CEAP2020.csv', sep=';', dtype=types)
+
+def df_2021():
+    types = {
+        'cpf': 'category',
+        'vlrLiquido': 'float'
+    }
+    return pd.read_csv(r'C:\Users\liora\Downloads\CEAP2021.csv', sep=';', dtype=types)
