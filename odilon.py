39a54e530fe9dcd1d4e7cf3ac3fc8f1ebfb789ef
Odilon-Jr <odiloncavalcantejunior@gmail.com>
2022-03-16 22:19:15 -0300

    Resetando documento.

--- odilon.py
+++ odilon.py
@@ -1,12 +1,1 @@
-from calendar import c
-
-
-teste
-pygame.examples.testsprite.main(update_rects = True, use_static = False, use_FastRenderGroup = False, screen_dims = [640, 480], use_alpha = False, flags = 0)c
-ddsfdsfsd
-ds
-cd
-sc
-sdc
-dscdc
-sdcs
+#Aqui colocar as funções
